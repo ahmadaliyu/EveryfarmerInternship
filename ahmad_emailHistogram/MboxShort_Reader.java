6ad203e79b6af96dc96ca7feeca0cd4f4c94131f
aahmadali <aliyuahmad1996@gmail.com>
2020-09-15 21:07:52 +0100

    creating an email histogram

--- ahmad_emailHistogram/MboxShort_Reader.java
+++ ahmad_emailHistogram/MboxShort_Reader.java
@@ -11,39 +11,31 @@ public class MboxShort_Reader {
         File file = new File("C:\\Users\\Ahmadee\\IdeaProjects\\Amad_StudentRecord\\mbox-short.txt");
         FileReader fr = new FileReader(file);
         BufferedReader bfr = new BufferedReader(fr);
-        StringBuffer sb = new StringBuffer();
 
         Map<String, Integer> mapContent = new HashMap<>();
         String line;
         List<String> list = new ArrayList<>();
 
         while ((line = bfr.readLine()) != null){
-//            bfr.skip(3);
-            if (line.contains("by") || line.contains("by".toUpperCase())) {
-                list.add(line);
-//                sb.append(line);
+            line = line.trim().toLowerCase();
+
+            if (line.contains("by") && !(line.contains("this")) && !(line.contains("component"))) {
+                line = line.replaceAll("by\\s", "");
+               String[] arr =  line.split("\\s+");
+                if (mapContent.containsKey(arr[0])){
+                    mapContent.put(arr[0], mapContent.get(arr[0]) + 1);
+                }else{
+                mapContent.put(arr[0], 1);
             }
-            sb.append("\n");
+
+            }
         }
 
-        System.out.println(mapContent.get(line));
-
-        // putting them in a list and then putting them in a map to count repeated keys
-        for (String ls: list){
-            if(mapContent.containsKey(ls)){
-                mapContent.put(ls, mapContent.get(ls) + 1);
-            }else{
-                mapContent.put(ls, 1);
-            }
-//            System.out.println(ls);
-        }
         // iterating the map
         Set<Map.Entry<String,Integer>> entries = mapContent.entrySet();
         for (Map.Entry entr: entries){
             System.out.println(entr.getKey() + " " + entr.getValue());
         }
         fr.close();
-        System.out.println(sb.toString());
-
     }
 }
